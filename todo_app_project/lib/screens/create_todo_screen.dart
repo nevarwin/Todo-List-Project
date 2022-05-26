39e58a4b1a790bbc9367120583686f2ff3d0bf9a
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-26 20:47:44 +0800

    added appbar

--- todo_app_project/lib/screens/create_todo_screen.dart
+++ todo_app_project/lib/screens/create_todo_screen.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 class CreateTodoScreen extends StatelessWidget {
   const CreateTodoScreen({Key? key}) : super(key: key);
 
+  static const routeName = './createTodoScreen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Todo Screen'),
+      ),
       body: Center(
         child: Text('Create Todo List'),
       ),
