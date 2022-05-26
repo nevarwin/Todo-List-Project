f4e8ec8f31d9081f9d2f262c2063291bdf78c588
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-26 19:24:16 +0800

    created create todo screen

---
+++ todo_app_project/lib/screens/create_todo_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateTodoScreen extends StatelessWidget {
+  const CreateTodoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Create Todo List'),
+      ),
+    );
+  }
+}
