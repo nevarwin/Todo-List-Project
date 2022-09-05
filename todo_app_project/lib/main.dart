a129ed18cc10fe4769ae5c2b502b1b2142195cca
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2022-09-05 22:22:38 +0800

    indention

--- todo_app_project/lib/main.dart
+++ todo_app_project/lib/main.dart
@@ -50,8 +50,8 @@ class MyApp extends StatelessWidget {
         // To use the playground font, add GoogleFonts package and uncomment
         fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
-// If you do not have a themeMode switch, uncomment this line
-// to let the device system mode control the theme mode:
+      // If you do not have a themeMode switch, uncomment this line
+      // to let the device system mode control the theme mode:
       themeMode: ThemeMode.system,
 
       home: const MyHomePage(),
