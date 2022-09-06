fa84bf5f6617991057eb55089b13f305886f8746
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2022-09-06 20:37:18 +0800

    fix date

--- todo_app_project/lib/screens/edit_todo_screen.dart
+++ todo_app_project/lib/screens/edit_todo_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import '../provider/todo.dart';
 
@@ -13,11 +14,18 @@ class EditTodoScreen extends StatefulWidget {
 }
 
 class _EditTodoScreenState extends State<EditTodoScreen> {
+  final _formGlobalKey = GlobalKey<FormState>();
   final _sizedboxWidth = 8.0;
   DateTime? _choosenDate;
   var dateLabel = 'Add date';
 
+  Todo _todoTemplate = Todo(
+    title: null,
+    description: null,
+  );
+
   void _setDate() {
+    debugPrint('trigger setdate');
     showDatePicker(
       context: context,
       initialDate: DateTime.now(),
@@ -37,79 +45,146 @@ class _EditTodoScreenState extends State<EditTodoScreen> {
   Widget build(BuildContext context) {
     final _todo = ModalRoute.of(context)!.settings.arguments as Todo;
 
+    if (_choosenDate == null && _todo.date == null) {
+      dateLabel;
+    } else if (_choosenDate != null) {
+      dateLabel = DateFormat.MMMEd().format(_choosenDate!);
+    } else {
+      dateLabel = DateFormat.MMMEd().format(_todo.date!);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit Task'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _formGlobalKey.currentState?.save();
+
+              Provider.of<TodoProvider>(
+                context,
+                listen: false,
+              ).updateTodo(
+                _todo.id!,
+                _todoTemplate,
+                _choosenDate,
+              );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Successful'),
+                  duration: Duration(seconds: 1),
+                ),
+              );
+
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.save_rounded),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              initialValue: _todo.title,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 0.0,
-                    style: BorderStyle.none,
+      body: Form(
+        key: _formGlobalKey,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                initialValue: _todo.title,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 0.0,
+                      style: BorderStyle.none,
+                    ),
                   ),
                 ),
-              ),
-              style: const TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
-              children: [
-                const Icon(
-                  Icons.menu_rounded,
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
                 ),
-                SizedBox(
-                  width: _sizedboxWidth,
-                ),
-                Expanded(
-                  child: TextFormField(
-                    initialValue: _todo.description ?? '',
-                    decoration: const InputDecoration(
-                      hintText: 'Add details',
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 0.0,
-                          style: BorderStyle.none,
+                onSaved: (newValue) {
+                  _todoTemplate = Todo(
+                    id: _todo.id,
+                    title: newValue,
+                    description: _todoTemplate.description,
+                    date: _todo.date,
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.menu_rounded,
+                    ),
+                    SizedBox(
+                      width: _sizedboxWidth,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: _todo.description,
+                        decoration: const InputDecoration(
+                          hintText: 'Add details',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 0.0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
                         ),
+                        onSaved: (newValue) {
+                          if (newValue != '') {
+                            _todoTemplate = Todo(
+                              id: _todo.id,
+                              title: _todoTemplate.title,
+                              description: newValue,
+                              date: _todo.date,
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                  ),
+                  onPressed: _setDate,
+                  icon: const Icon(
+                    Icons.edit_calendar,
+                    color: Colors.black87,
+                  ),
+                  label: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      dateLabel,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.normal,
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            TextButton.icon(
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.zero,
               ),
-              onPressed: _setDate,
-              icon: const Icon(
-                Icons.edit_calendar,
-                color: Colors.black87,
-              ),
-              label: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  _todo.date == null
-                      ? dateLabel
-                      : DateFormat.MMMEd().format(_todo.date!),
-                  style: const TextStyle(
-                    color: Colors.black87,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
+
+//   String buildText() {
+//     if (_choosenDate != null) {
+//       setState(() {});
+//     }
+//     return 'Add date';
+//   }
 }
